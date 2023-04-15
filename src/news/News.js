52874d2d05fe13a27481598703ddde30c8188536
xuanjian zhang <xzha0383@student.monash.edu>
2023-04-15 16:25:15 +0800

    make home page buttons work

--- src/news/News.js
+++ src/news/News.js
@@ -15,7 +15,7 @@ function News(props) {
     </div>
     <div className="NewsContent">
         <p>{props.description}</p>
-        <button href={props.link} rel="noopener noreferrer">Explore ></button>
+        <button onClick={() => window.location.href = props.link}>Explore ></button>
     </div>
     </Fade>
   </div>
