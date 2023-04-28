f8ccbb88e115ec83d4206d45ddaffac9b4a363d8
app <xzha0355@student.monash.edu>
2023-04-28 13:32:20 +1000

    header fix

--- src/navigationBar/NavigationBar.js
+++ src/navigationBar/NavigationBar.js
@@ -6,6 +6,11 @@ import { useState } from 'react';
 function NavigationBar() {
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
   const [isClickedFolds, setIsClickedFolds] = useState(false);
+  const [currentUrl, setCurrentUrl] = useState(window.location.pathname);
+
+  function handleClick(e) {
+    setCurrentUrl(e.target.pathname);
+  }
 
   function handleDropdownToggle() {
     setIsDropdownVisible(!isDropdownVisible);
@@ -53,54 +58,66 @@ function NavigationBar() {
 //        <i className={isClickedFolds? "fas fa-times":"fas fa-bars"}></i>
 //      </div>
 //    </nav>
-    <div className="Navbar">
-        <div className="leftSide">
-            <h1>Green Journey</h1>
-        </div>
-        <div className="rightSide">
-            <ul id={isClickedFolds?"show":""}>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/footprint">Footprint</a>
-            </li>
-
-
-
-
-            {/* <li>
-              <a href="/quiz">Quiz</a>
-            </li>  */}
-
-            <li>
-              <a href="/travel">Travel</a>
-            </li>
-
-              {/* <li>
-              <a href="/test">Test</a>
-            </li>  */}
-
-            <li>
-              <a href="/education">Education</a>
-            </li>
-            <li className="dropdown" onMouseEnter={handleDropdownToggle} onMouseLeave={handleDropdownToggle}>
-              <a>Information</a>
-              <ul className={isDropdownVisible ? "visible" : "hidden"}>
-                <li><a href="/information/state">State Information</a></li>
-                <li><a href="/information/transport">Transport Information</a></li>
-              </ul>
-            </li>
-           </ul>
-
-            
-
-            <div id="mobile" onClick = {() => setIsClickedFolds(!isClickedFolds)}>
-              <i className={isClickedFolds? "fas fa-times":"fas fa-bars"}></i>
-            </div>
-        </div>
-    </div>
-    )
+<div className="Navbar">
+<div className="leftSide">
+  <h1>Green Journey</h1>
+</div>
+<div className="rightSide">
+  <ul>
+    <li>
+      <a
+        href="/"
+        className={currentUrl === "/" ? "active" : ""}
+        onClick={handleClick}
+      >
+        Home
+      </a>
+    </li>
+    <li>
+      <a
+        href="/footprint"
+        className={currentUrl === "/footprint" ? "active" : ""}
+        onClick={handleClick}
+      >
+        Footprint
+      </a>
+    </li>
+    <li>
+      <a
+        href="/travel"
+        className={currentUrl === "/travel" ? "active" : ""}
+        onClick={handleClick}
+      >
+        Travel
+      </a>
+    </li>
+    <li>
+      <a
+        href="/education"
+        className={currentUrl === "/education" ? "active" : ""}
+        onClick={handleClick}
+      >
+        Education
+      </a>
+    </li>
+    <li className="dropdown">
+      <a>Information</a>
+      <ul className={isDropdownVisible ? "visible" : "hidden"}>
+        <li>
+          <a href="/information/state">State Information</a>
+        </li>
+        <li>
+          <a href="/information/transport">Transport Information</a>
+        </li>
+      </ul>
+    </li>
+  </ul>
+  <div id="mobile" onClick={() => setIsClickedFolds(!isClickedFolds)}>
+    <i className={isClickedFolds ? "fas fa-times" : "fas fa-bars"}></i>
+  </div>
+</div>
+</div>
+    );
 }
 
 export default NavigationBar;
