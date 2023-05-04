da5a2171ae49740436932aced42ff9a0ee9bc31d
kefanzhang123 <110731715+kefanzhang123@users.noreply.github.com>
2023-05-04 21:09:25 +1000

    description

--- src/description/Description.js
+++ src/description/Description.js
@@ -3,8 +3,10 @@ import Alert from 'react-bootstrap/Alert';
 function Description() {
     return (
         <Alert variant="primary" style={{textAlign: 'center'}}>
-          In this interface, you can view the carbon emissions of each state in Australia, which can help you understand the state of each state.
+          In this interface, you can view the carbon emissions of each state in Australia.
+          By comparing the carbon emission data of different regions, people can also know which regions need to work harder to reduce carbon emissions, and can learn from the successful experience of other regions, so as to reduce their own carbon emissions more effectively. Therefore, providing accurate and reliable carbon emission data can help people realize the urgency of emission reduction, thereby promoting them to take active emission reduction actions.
         </Alert>
+        
       );
 }
 
